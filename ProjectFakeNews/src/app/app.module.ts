0896155db4183d096fb22b53205cd20b5a3149e7
Marcelo Pena Vila <marcelopena2010@gmail.com>
2020-07-31 20:55:30 -0300

    - Criação do Projeto Fake News para Hackathon

--- ProjectFakeNews/src/app/app.module.ts
+++ ProjectFakeNews/src/app/app.module.ts
@@ -3,30 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PortalSearchComponent } from './portal-search/portal-search.component';
-import { PeopleMenuComponent } from './people-menu/people-menu.component';
-import { RouterModule, Routes} from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-const appRoutes: Routes = [
-  {path: '', redirectTo: 'search', pathMatch: 'full' }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PortalSearchComponent,
-    PeopleMenuComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    ),
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
