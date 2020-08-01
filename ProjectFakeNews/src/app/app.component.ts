55b38380bcf6a1220290952a04d888935a8eef36
Marcelo Pena Vila <marcelopena2010@gmail.com>
2020-07-31 21:11:00 -0300

    - Correção.

--- ProjectFakeNews/src/app/app.component.ts
+++ ProjectFakeNews/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'ProjectFakeNews';
+export class AppComponent  {
+    
+  onSubmit(myForm: NgForm) {
+    myForm.resetForm({
+      name: myForm.value
+    })
+  }
+  
 }
