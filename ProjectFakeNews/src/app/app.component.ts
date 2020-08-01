dcf5e79eddc38e15b381b08ebce32fdd1f20a1dd
Marcelo Pena Vila <marcelopena2010@gmail.com>
2020-07-31 22:00:06 -0300

    - Melhoria visual do portal.

--- ProjectFakeNews/src/app/app.component.ts
+++ ProjectFakeNews/src/app/app.component.ts
@@ -11,8 +11,26 @@ export class AppComponent  {
   onSubmit(myForm: NgForm) {
     let json = JSON.stringify(myForm.value);
     myForm.resetForm({
-      name: json
+      search: json
     })
   }
   
 }
+/*
+*the rest of the imports are here don't worry*
+import { ApiService } from '@api/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class homeComponent implements OnInit {
+
+constructor( private ApiService: ApiService){
+
+ JSON = this.ApiService.getParticularData();
+ console.log(JSON) // returns undefined
+
+}
+}*/
